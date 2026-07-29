86a0796431d3d3c8355b128da354de592376ae21
Sergio Murilo Macedo <sergio.murilo.macedo@gmail.com>
2026-07-29 19:18:14 -0300

    feat: create repositories

---
+++ src/main/java/br/com/sergio/gestaopedidos/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package br.com.sergio.gestaopedidos.repository;
+
+import br.com.sergio.gestaopedidos.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
