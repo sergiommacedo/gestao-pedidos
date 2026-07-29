a75fb9bcb17ebb91c6340e9086ab53c452550c89
Sergio Murilo Macedo <sergio.murilo.macedo@gmail.com>
2026-07-29 17:31:55 -0300

    chore: initialize Spring Boot project

---
+++ src/test/java/br/com/sergio/gestaopedidos/GestaoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sergio.gestaopedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPedidosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
