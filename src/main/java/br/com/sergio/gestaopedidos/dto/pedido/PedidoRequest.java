5e058481783fe77b275dc0fe75c8c0dce636b402
Sergio Murilo Macedo <sergio.murilo.macedo@gmail.com>
2026-07-30 10:30:39 -0300

    feat: implement order domain and business rules

--- src/main/java/br/com/sergio/gestaopedidos/dto/pedido/PedidoRequest.java
+++ src/main/java/br/com/sergio/gestaopedidos/dto/pedido/PedidoRequest.java
@@ -3,8 +3,10 @@ package br.com.sergio.gestaopedidos.dto.pedido;
 import br.com.sergio.gestaopedidos.enums.FormaPagamento;
 import br.com.sergio.gestaopedidos.enums.TipoEntrega;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 import java.math.BigDecimal;
@@ -22,8 +24,16 @@ public record PedidoRequest(
         @NotNull(message = "Tipo de entrega é obrigatório.")
         TipoEntrega tipoEntrega,
 
+        @DecimalMin(
+                value = "0.00",
+                message = "Taxa de entrega não pode ser negativa."
+        )
         BigDecimal taxaEntrega,
 
+        @Size(
+                max = 500,
+                message = "Observação deve possuir no máximo 500 caracteres."
+        )
         String observacao,
 
         @Valid
